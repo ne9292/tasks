2790ce9640aeb1a18e36cfcc23f3baa9b58f48fa
נחמי הלפר <n0556793700@gmail.com>
2026-04-15 02:07:11 +0300

    fix a

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -1,27 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using Microsoft.Extensions.Configuration; // חובה להוסיף
 
 namespace TodoApi;
 
 public partial class ToDoDbContext : DbContext
 {
-    public ToDoDbContext()
-    {
-    }
+    public ToDoDbContext() { }
 
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-        : base(options)
-    {
-    }
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) { }
 
     public virtual DbSet<Item> Items { get; set; }
-
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // טעינת הגדרות מתוך משתני סביבה (כמו ב-Render) או מקובץ הגדרות
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables() // שורת הקסם שמושכת את הנתונים מ-Render
+                .Build();
+
+            // הוא יחפש קודם כל משתנה בשם ToDoDB ב-Environment Variables
+            var connectionString = configuration.GetConnectionString("ToDoDB");
+
+            optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -32,18 +41,14 @@ public partial class ToDoDbContext : DbContext
         modelBuilder.Entity<Item>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("items");
-
             entity.Property(e => e.Name).HasMaxLength(100);
         });
 
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("users");
-
             entity.Property(e => e.Password).HasMaxLength(100);
             entity.Property(e => e.Username).HasMaxLength(100);
         });
